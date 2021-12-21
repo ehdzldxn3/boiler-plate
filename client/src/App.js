1860a871617ab610c021365cdbe8a78f2c64f4ea
ehdzldxn3 <77223991+ehdzldxn3@users.noreply.github.com>
2021-12-21 20:31:12 +0900

    새로 만들어야함

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage'
-import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import SignUp from './components/views/SignUp/SignUp'
 import Navbar from './components/views/NavBar/Navbar'
 import Auth from './hoc/auth'
 
@@ -18,7 +18,7 @@ function App() {
         <Route path='/login' component={Auth(LoginPage, false)} />
 
         {/*  */}
-        <Route path='/register' component={Auth(RegisterPage, false) } />
+        <Route path='/signUp' component={Auth(SignUp, false) } />
 
       </Switch>
     </Router>
